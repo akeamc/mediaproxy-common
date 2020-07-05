7504578c530dff712f6ea4c4ce59344851e120e2
Åke Amcoff <ake.amcoff@gmail.com>
2020-07-05 12:37:39 +0200

    Documentation (mostly) (#12)
    * Bump serde_json from 1.0.55 to 1.0.56
    
    Bumps [serde_json](https://github.com/serde-rs/json) from 1.0.55 to 1.0.56.
    - [Release notes](https://github.com/serde-rs/json/releases)
    - [Commits](https://github.com/serde-rs/json/compare/v1.0.55...v1.0.56)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update CI to match mediaproxy-server
    
    * Add documentation
    
    * cargo fmt
    
    * Fix doc code
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/query.rs
+++ src/query.rs
@@ -15,6 +15,7 @@ pub enum ImageProcessingOutput {
     Gif,
 }
 
+/// The default URL-safe base64 configuration.
 fn b64_config() -> base64::Config {
     base64::Config::new(base64::CharacterSet::UrlSafe, false)
 }
@@ -25,20 +26,43 @@ custom_error! {pub QueryFingerprintConversionError
   UnicodeError{source: std::str::Utf8Error} = "Could not convert byte array to string!"
 }
 
+/// The default object that clients use to make requests to Media Proxy.
 #[derive(Serialize, Deserialize)]
 pub struct Query {
+    /// URL of the source image. Input formats currently supported are the same as those of the [image] crate.
     pub source: String,
+
+    /// The width of the processed image.
     pub width: Option<u32>,
+
+    /// The height of the processed image.
     pub height: Option<u32>,
+
+    /// Output format of the image.
     pub format: ImageProcessingOutput,
 }
 
 impl Query {
+    /// Convert a `Query` to a fingerprint.
+    ///
+    /// In reality, this just serializes the `Query` to JSON and encodes it in base64.
     pub fn to_fingerprint(self: &Self) -> String {
         let json = serde_json::to_string(&self).unwrap();
         encode_config(json, b64_config())
     }
 
+    /// Create a `Query` from a base64 encoded fingerprint. Fingerprints can be created with `Query.to_fingerprint()`.
+    ///
+    /// # Arguments
+    ///
+    /// * `fingerprint` - A base64 encoded `Query` in JSON.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use mediaproxy_common::query::Query;
+    /// let query = Query::from_fingerprint("eyJzb3VyY2UiOiJodHRwczovL2R1bW15aW1hZ2UuY29tLzYwMHg0MDAvMDAwL2ZmZiIsIndpZHRoIjpudWxsLCJoZWlnaHQiOm51bGwsImZvcm1hdCI6ImpwZWcifQ".to_string());
+    /// ```
     pub fn from_fingerprint(fingerprint: String) -> Result<Query, QueryFingerprintConversionError> {
         let bytes = base64::decode_config(fingerprint, b64_config())?;
         let json = std::str::from_utf8(&bytes)?;
